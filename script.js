500f4d85349375c2cf7d65469632d752354af067
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2025-07-02 18:32:14 +0530

    Favicon updated

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-var tl = gsap.timeline();
-tl.from(".line h1,h2", {
-  y: 150,
-  stagger: 0.25,
-  duration: 0.6,
-  delay: 0.5,
-});
+function loadingAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".line h1,h2", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
 
-tl.from("#line1-part1", {
-  opacity: 0,
-  onStart: function () {
-    var h5timer = document.querySelector("#line1-part1 h5");
-    var grow = 0;
-
-    setInterval(function () {
-      if (grow < 100) {
-        grow++;
-        h5timer.innerHTML = grow;
-      } else {
-        h5timer.innerHTML = "100";
-      }
-    }, 30);
-  },
-});
-
-
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.2,
-  delay: 3.8,
-});
-
-tl.from("#page1",{
-    delay:0.2,
-    y:1600,
+  tl.from("#line1-part1", {
     opacity: 0,
-    ease:Power4,
-    duration:0.5,
-})
-tl.to("#loader", {
-  display: "none",
-});
+    onStart: function () {
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
+
+      setInterval(function () {
+        if (grow < 100) {
+          grow++;
+          h5timer.innerHTML = grow;
+        } else {
+          h5timer.innerHTML = "100";
+        }
+      }, 30);
+    },
+  });
+
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 3.8,
+  });
+
+  tl.from("#page1", {
+    delay: 0.2,
+    y: 1600,
+    opacity: 0,
+    ease: Power4,
+    duration: 0.5,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+}
+loadingAnimation();
